054444ce93606aa623592035a13073a17f825dcf
rdeepak2002 <rdeepak2002@gmail.com>
2021-08-02 03:39:39 -0400

    adding project cards

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -1,3 +1,5 @@
+import { Container } from "react-bootstrap";
+
 interface ProjectsProps {
   isMobileCss: boolean
   showAll: boolean
@@ -5,30 +7,67 @@ interface ProjectsProps {
 
 const Projects = (props: ProjectsProps) => {
   return (
-    <div style={{ display: "grid", gridAutoRows: "minmax(100px, auto)", gridTemplateColumns: props.isMobileCss ? "repeat(1, 1fr)" : "repeat(2, 1fr)" }}>
-      <p>
-        asdf
-      </p>
-      <p>
-        asdf
-      </p>      <p>
-        asdf
-      </p>      <p>
-        asdf
-      </p>      <p>
-        asdf
-      </p>
+    <div
+      style={{
+        display: "grid",
+        gridAutoRows: "minmax(100px, auto)",
+        gridTemplateColumns: props.isMobileCss ? "repeat(1, 1fr)" : "repeat(2, 1fr)",
+        columnGap: "1rem",
+        rowGap: "1rem"
+      }}
+    >
+      <Project
+        title={"Teaching Computer Science At Indian Village"}
+        type={"Volunteering"}
+        date={"July of 2019"}
+        content={
+          <div>hello world</div>
+        }
+        tags={[]}
+      />
+
+      <Container style={{ backgroundColor: "red" }}>
+        <h5>Hello World</h5>
+      </Container>
+      <Container style={{ backgroundColor: "blue" }}>
+        <h5>Hello World</h5>
+      </Container>
+      <Container style={{ backgroundColor: "green" }}>
+        <h5>Hello World</h5>
+      </Container>
+      <Container style={{ backgroundColor: "pink" }}>
+        <h5>Hello World</h5>
+      </Container>
+      <Container style={{ backgroundColor: "purple" }}>
+        <h5>Hello World</h5>
+      </Container>
 
     </div>
   );
 }
 
 interface ProjectProps {
-
+  imageUrl?: string
+  videoUrl?: string
+  title: string
+  type: string
+  date: string
+  content: any
+  tags: Array<string>
 }
 
 const Project = (props: ProjectProps) => {
-
+  return (
+    <Container className="card" style={{ width: "100%" }}>
+      <h4 style={{ marginTop: "1rem" }}>{props.title}</h4>
+      <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+        <p style={{color: "#0078ff"}}>{props.type}</p> <p style={{marginLeft: 10, marginRight: 10, color: "rgba(50, 50, 50)"}}>/</p> <p style={{color: "rgba(50, 50, 50)"}}>{props.date}</p>
+      </div>
+      <div style={{marginBottom: "1.5rem"}}>
+        {props.content}
+      </div>
+    </Container>
+  );
 }
 
 export default Projects;
