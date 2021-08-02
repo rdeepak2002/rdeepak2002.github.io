12a61f8c6e8c86f1641aa0173439f44d0ffdb3e7
rdeepak2002 <rdeepak2002@gmail.com>
2021-08-02 04:08:44 -0400

    added show more show less feature

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -1,4 +1,5 @@
-import { Container, Image } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Container, Image } from "react-bootstrap";
 
 interface ProjectsProps {
   isMobileCss: boolean
@@ -6,11 +7,13 @@ interface ProjectsProps {
 }
 
 const Projects = (props: ProjectsProps) => {
+  const showMoreHeight = "100px";
+
   return (
     <div
       style={{
         display: "grid",
-        gridAutoRows: "minmax(100px, auto)",
+        gridAutoRows: `minmax(${showMoreHeight}, auto)`,
         gridTemplateColumns: props.isMobileCss ? "repeat(1, 1fr)" : "repeat(2, 1fr)",
         columnGap: "1rem",
         rowGap: "1rem"
@@ -22,9 +25,24 @@ const Projects = (props: ProjectsProps) => {
         type={"Volunteering"}
         date={"July of 2019"}
         content={
-          <div>hello world</div>
+          <>
+            <div>hello world</div>
+            <div>hello world</div>
+            <div>hello world</div>
+            <div>hello world</div>
+            <div>hello world</div>
+            <div>hello world</div>
+            <div>hello world</div>
+            <div>hello world</div>
+            <div>hello world</div>
+            <div>hello world</div>
+            <div>hello world</div>
+            <div>hello world</div>
+            <div>hello world</div>
+          </>
         }
         tags={[]}
+        showMoreHeight={showMoreHeight}
       />
 
       <Container style={{ backgroundColor: "red" }}>
@@ -55,9 +73,13 @@ interface ProjectProps {
   date: string
   content: any
   tags: Array<string>
+  showMoreHeight: number | string
 }
 
 const Project = (props: ProjectProps) => {
+  const showMoreHeight = props.showMoreHeight;
+  const [showMore, setShowMore] = useState<boolean>(false);
+
   return (
     <Container className="card" style={{ width: "100%", padding: 0 }}>
       {props.imageUrl &&
@@ -66,13 +88,38 @@ const Project = (props: ProjectProps) => {
       {props.videoUrl &&
         <iframe width="100%" height="300" src={props.videoUrl} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
       }
-      <div style={{paddingLeft: 15, paddingRight: 15}}>
+      <div style={{ paddingLeft: 15, paddingRight: 15 }}>
         <h4 style={{ marginTop: "1rem" }}>{props.title}</h4>
         <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
           <p style={{ color: "#0078ff" }}>{props.type}</p> <p style={{ marginLeft: 10, marginRight: 10, color: "rgba(50, 50, 50)" }}>/</p> <p style={{ color: "rgba(50, 50, 50)" }}>{props.date}</p>
         </div>
-        <div style={{ marginBottom: "1.5rem" }}>
+        <div style={{ marginBottom: "1rem", paddingLeft: 15, paddingRight: 15, overflow: "hidden", maxHeight: showMore ? "999999px" : showMoreHeight }}>
           {props.content}
+        </div>
+        <div style={{ marginBottom: "1.5rem", display: "flex", alignItems: "center", justifyContent: "center" }}>
+          {
+            showMore
+              ?
+              (
+                <Button
+                  onClick={() => {
+                    setShowMore(false);
+                  }}
+                >
+                  Show Less
+                </Button>
+              )
+              :
+              (
+                <Button
+                  onClick={() => {
+                    setShowMore(true);
+                  }}
+                >
+                  Show More
+                </Button>
+              )
+          }
         </div>
       </div>
     </Container>
