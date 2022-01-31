d0063daa0b3c0209af7bcbcfe73715d036c5aca0
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-01-31 15:37:41 -0500

    updated profile image

--- src/components/home/AboutSection.tsx
+++ src/components/home/AboutSection.tsx
@@ -1,5 +1,5 @@
 import {Image} from "react-bootstrap";
-import deepak_picture from "resources/images/deepak_2.jpeg";
+import deepak_picture from "resources/images/deepak_3.jpeg";
 
 interface AboutSectionProps {
     isMobileCss: boolean;
